d09bae0c57ba799a1fd160ad934d4f491fc7023c
Bombele Liyama Camille <94522767+Bombele@users.noreply.github.com>
2025-11-22 17:22:14 -0400

    Update abuse_types.py

--- abuse_types.py
+++ abuse_types.py
@@ -5,3 +5,69 @@ abuse_types = [
     "Abus institutionnel",
     "Abus économique"
 ]
+abuse_types = [
+    {
+        "code": "AB-J01",
+        "ln": "Kokanga makambo na tribunal",
+        "sw": "Kucheleweshwa kwa kesi",
+        "en": "Procedural delays",
+        "fr": "Retards procéduraux",
+        "es": "Retrasos procesales"
+    },
+    {
+        "code": "AB-S02",
+        "ln": "Kobanga lisungi ya lopitalo",
+        "sw": "Ukosefu wa huduma za afya",
+        "en": "Lack of medical care",
+        "fr": "Manque de soins médicaux",
+        "es": "Falta de atención médica"
+    },
+    {
+        "code": "AB-A03",
+        "ln": "Kopesa mbongo na ba fonctionnaire",
+        "sw": "Kutishwa na maafisa",
+        "en": "Extortion by officials",
+        "fr": "Extorsion par des fonctionnaires",
+        "es": "Extorsión por parte de funcionarios"
+    },
+    {
+        "code": "AB-I04",
+        "ln": "Kozanga bosoto na bokonzi",
+        "sw": "Matumizi mabaya ya mamlaka",
+        "en": "Abuse of power",
+        "fr": "Abus de pouvoir",
+        "es": "Abuso de poder"
+    },
+    {
+        "code": "AB-J05",
+        "ln": "Koboya bato na tribunal",
+        "sw": "Ubaguzi wa kimahakama",
+        "en": "Judicial discrimination",
+        "fr": "Discrimination judiciaire",
+        "es": "Discriminación judicial"
+    },
+    {
+        "code": "AB-A06",
+        "ln": "Kopesa mbongo na ba bureau",
+        "sw": "Rushwa ya kiutawala",
+        "en": "Administrative corruption",
+        "fr": "Corruption administrative",
+        "es": "Corrupción administrativa"
+    },
+    {
+        "code": "AB-J07",
+        "ln": "Kokanga nzela ya bosembo",
+        "sw": "Kuzuia haki",
+        "en": "Obstruction of justice",
+        "fr": "Entrave à la justice",
+        "es": "Obstrucción de justicia"
+    },
+    {
+        "code": "AB-X99",
+        "ln": "Ba abus mosusu",
+        "sw": "Madhulumu mengine",
+        "en": "Other abuses",
+        "fr": "Autres abus",
+        "es": "Otros abusos"
+    }
+]
